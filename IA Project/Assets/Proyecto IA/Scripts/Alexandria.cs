41a5e8b5554f5272e32e2b69666a7fd6690b87df
MoonAntonio <pinchodev91@gmail.com>
2017-03-02 00:12:32 +0100

    Iniciando proyecto

---
+++ IA Project/Assets/Proyecto IA/Scripts/Alexandria.cs
@@ -0,0 +1,20 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Alexandria.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (Moon Pincho) 									        \\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonPincho
+{
+	public class Alexandria : MonoBehaviour 
+	{
+
+	}
+}
